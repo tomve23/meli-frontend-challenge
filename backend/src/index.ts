0d4ee3480dcfb8021aaf361c38251d47ec6c6aa1
Tomás Vera <tomas.vera@bigbox.com.ar>
2025-03-30 19:02:09 -0300

    feat: initialized express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello, world!!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const server = http.createServer(app);
+
+server.listen(9080, () => {
+  console.log("Server is running on port 9080");
+});
