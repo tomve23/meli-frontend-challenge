4d388f8976fc04ae81b9dd73e2a348142bb3f402
Tomás Vera <tomas.vera@bigbox.com.ar>
2025-04-06 02:00:08 -0300

    feat: initialized frontend project

---
+++ frontend/src/main.tsx
@@ -0,0 +1,5 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@/assets/styles/index.css";
+
+createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
