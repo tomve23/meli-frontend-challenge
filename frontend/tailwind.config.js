03c80d547de9f9839100e9caa818b0702e839bf2
Tomás Vera <tomas.vera@bigbox.com.ar>
2025-04-07 02:44:49 -0300

    feat: setup styles configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ['class'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,css}'],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {
+        primary: '#fee600',
+        secondary: {
+          DEFAULT: '#3483fa',
+          hover: '#2968c8',
+        },
+        placeholder: '#999999',
+        background: {
+          DEFAULT: '#eeeeee',
+          hover: '#dddddd',
+        },
+        foreground: '#666666',
+        border: 'hsl(0 0% 89.8%)',
+        input: 'hsl(0 0% 89.8%)',
+        ring: 'hsl(0 0% 3.9%)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      include: "**/*.svg?react",
+    }),
+  ],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
