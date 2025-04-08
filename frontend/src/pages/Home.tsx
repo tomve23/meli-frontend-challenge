9f17ae7af66fd90ae9b3f859b93ac2fb5d2d8bee
Tomás Vera <tomas.vera@bigbox.com.ar>
2025-04-08 18:48:55 -0300

    feat: cleaned up home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import LogoMeliBig from '@/assets/images/LogoMeliBig.png';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the home page of our application.</p>
+    <div className="flex flex-col items-center justify-center container mx-auto mt-16 p-9 bg-white rounded-sm">
+      <img src={LogoMeliBig} alt="Logo" className="mb-4 h-20" />
+      <h1 className="text-3xl text-secondary">Test Práctico | Frontend</h1>
     </div>
   );
 };
